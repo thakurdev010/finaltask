57384e0e713b7113d60e74b1ecb33b99c0780f5e
thakurdev010 <thakurdev009@gmail.com>
2017-07-13 14:21:28 +0530

    first commit

---
+++ R/function_real_roots.R
@@ -0,0 +1,20 @@
+
+real.roots <- function(a, b, c)
+{
+  if (a == 0.)
+    stop("Leading term cannot be zero")
+  
+  d = b*b - 4*a*c # discriminant
+  
+  if (d < 0)
+    rr = c()
+  else if (d == 0)
+    rr = c( -b/(2*a) )
+  else
+    rr = c( (-b - sqrt(d))/(2*a),
+            (-b + sqrt(d))/(2*a)  )
+  
+  return(rr)
+}
+
+real.roots(1, 5, 6)
